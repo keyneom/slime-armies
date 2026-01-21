88978cabcf1cfbc2ef62b5e6f6779f4d6b406f2f
keyneom <mika@spire.dev>
2026-01-20 20:04:21 -0600

    Fix deployment

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-TRUNK_BUILD_MINIFY=false trunk build --release --dist docs
+TRUNK_BUILD_MINIFY=false trunk build --release --dist docs --public-url /slime-armies/
 touch docs/.nojekyll
