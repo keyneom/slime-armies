2d615e4d0d51f7ec68467616d4777bc521ccab2c
keyneom <mika@spire.dev>
2026-01-20 20:07:41 -0600

    Fixed?

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Unset color-related env vars that can cause build issues
+unset NO_COLOR FORCE_COLOR
+
 TRUNK_BUILD_MINIFY=false trunk build --release --dist docs --public-url /slime-armies/
 touch docs/.nojekyll
